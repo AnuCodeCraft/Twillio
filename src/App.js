19b9c2e5bcfe3d303baf55ae37e152428fe5fd43
anupriya12-code <anupriya10031@gmail.com>
2021-02-14 10:48:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Video Chat with React</h1>
+      </header>
+      
+      <footer>
+      <button >Connect</button>
+        <p>
+          Made with{' '}
+          <span role="img" aria-label="React">
+            ⚛️
+          </span>{' '}
+           <p>by anupriya</p>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
